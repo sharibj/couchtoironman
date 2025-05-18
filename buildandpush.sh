af4b6acc21ea5546bddb92e88c98b3487de12c9e
Sharib Jafari <sharib.jafari.extern@idealo.de>
2025-05-18 11:44:37 +0200

    Autocommit

--- buildandpush.sh
+++ buildandpush.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-node utils/build.js || { echo "Build failed"; exit 1; }
-echo "Build succeeded"
-
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa || { echo "SSH add failed"; exit 1; }
 
-git pull || { echo "Git pull fai,led"; exit 1; }
+git pull || { echo "Git pull failed"; exit 1; }
 echo "Git pull succeeded"
+
+node utils/build.js || { echo "Build failed"; exit 1; }
+echo "Build succeeded"
 
 git add . && git commit -m "Autocommit" || { echo "Git commit failed"; exit 1; }
 echo "Git commit succeeded"
